291ff7b95461ee7abb25c051942ed2392f517d9c
addyxx-h4ck1 <briannjosh23@gmail.com>
2024-05-04 19:01:25 +0300

    Updated the origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,9 @@
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://authentication-sys.netlify.app/',
+  'https://netlify.com/',
+  'https://www.netlify.com/',
+  'https://netlify.app/',
+]
 
 module.exports = { allowedOrigins }
