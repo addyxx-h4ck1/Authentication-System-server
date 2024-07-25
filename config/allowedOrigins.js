0b8c11f17625dcc6d4b2578eb6889274e972491c
addyxx-h4ck1 <briannjosh23@gmail.com>
2024-07-25 03:40:47 +0300

    add dev origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   'http://localhost:5173',
+  'http://localhost:3000',
   'https://authentication-sys.netlify.app',
   'authentication-sys.netlify.app/:1',
   'https://authentication-sys.vercel.app',
